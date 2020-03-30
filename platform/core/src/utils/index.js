f1405d34a060e6da2b02308aee2aef21e15a4510
biggfish3147 <biggfish3147@gmail.com>
2020-03-30 19:15:47 +0800

    fork原始版本

---
+++ platform/core/src/utils/index.js
@@ -0,0 +1,50 @@
+import ObjectPath from './objectPath';
+import StackManager from './StackManager.js';
+import absoluteUrl from './absoluteUrl';
+import addServers from './addServers';
+import guid from './guid';
+import sortBy from './sortBy.js';
+import studyMetadataManager from './studyMetadataManager';
+import writeScript from './writeScript.js';
+import DicomLoaderService from './dicomLoaderService.js';
+import b64toBlob from './b64toBlob.js';
+import loadAndCacheDerivedDisplaySets from './loadAndCacheDerivedDisplaySets.js';
+import * as urlUtil from './urlUtil';
+import makeCancelable from './makeCancelable';
+import hotkeys from './hotkeys';
+
+const utils = {
+  guid,
+  ObjectPath,
+  absoluteUrl,
+  addServers,
+  sortBy,
+  writeScript,
+  b64toBlob,
+  StackManager,
+  studyMetadataManager,
+  DicomLoaderService,
+  urlUtil,
+  loadAndCacheDerivedDisplaySets,
+  makeCancelable,
+  hotkeys,
+};
+
+export {
+  guid,
+  ObjectPath,
+  absoluteUrl,
+  addServers,
+  sortBy,
+  writeScript,
+  b64toBlob,
+  StackManager,
+  studyMetadataManager,
+  DicomLoaderService,
+  urlUtil,
+  loadAndCacheDerivedDisplaySets,
+  makeCancelable,
+  hotkeys,
+};
+
+export default utils;
---
+++ platform/core/src/utils/index.test.js
@@ -0,0 +1,26 @@
+import * as utils from './index.js';
+
+describe('Top level exports', () => {
+  test('should export the modules ', () => {
+    const expectedExports = [
+      'guid',
+      'ObjectPath',
+      'absoluteUrl',
+      'addServers',
+      'sortBy',
+      'writeScript',
+      'b64toBlob',
+      'StackManager',
+      'studyMetadataManager',
+      'loadAndCacheDerivedDisplaySets',
+      'DicomLoaderService',
+      'urlUtil',
+      'makeCancelable',
+      'hotkeys',
+    ].sort();
+
+    const exports = Object.keys(utils.default).sort();
+
+    expect(exports).toEqual(expectedExports);
+  });
+});
